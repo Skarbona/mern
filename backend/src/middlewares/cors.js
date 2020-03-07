ffb022862db8308669dc8b70f828d6852fb3e446
Filip <filip.sokolowski1990@gmail.com>
2020-03-07 16:05:56 +0100

    remove extras

--- backend/src/middlewares/cors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var corsHeaders = function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-    next();
-};
-exports["default"] = corsHeaders;
